d7b6b2236bc70b5dca9b54b3993d0aada43a40bc
Greg Bergé <neoziro@gmail.com>
2013-05-11 16:46:52 +0200

    add task checking

--- client/list/main.js
+++ client/list/main.js
@@ -1,1 +1,11 @@
+Template.list.events({
+  'change ul li [type=checkbox]': function (event) {
+    var $target = $(event.target),
+        id = $target.attr('name'),
+        done = $target.is(':checked');
+
+    Tasks.update({_id: id}, {$set: {done: done}});
+  }
+});
+
 Template.list.tasks = Tasks.find();

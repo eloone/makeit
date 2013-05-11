d35fae6d4d5abbf159dc77aa0051425328702157
Adlen Afane <adlen.afane@gmail.com>
2013-05-11 18:17:39 +0200

    Tags ok

--- client/tag/main.js
+++ client/tag/main.js
@@ -4,18 +4,19 @@ Template.listTag.tags = function () {
 	var total_count = 0;
 
 	Tasks.find().forEach(function (task) {
-	_.each(todo.tags, function (tag) {
-      var tag_info = _.find(tag_infos, function (x) { return x.tag === tag; });
-      if (! tag_info)
-        tag_infos.push({tag: tag, count: 1});
-      else
-        tag_info.count++;
-    });
+	_.each(task.tags, function (tag) {
+    if (tag != '') {
+      var tag_info = _.find(tag_infos, function (x) { return x.tags === tag; });
+        if (! tag_info)
+          tag_infos.push({text: tag, count: 1});
+        else
+          tag_info.count++;
+    }});
     total_count++;
   });
 
   tag_infos = _.sortBy(tag_infos, function (x) { return x.tag; });
-  tag_infos.unshift({tag: null, count: total_count});
+  // tag_infos.unshift({tag: null, count: total_count});
 
   return tag_infos;
 }

898ba76dabdc0c12fdadc77561d0bdf3d25a0b82
Adlen Afane <adlen.afane@gmail.com>
2013-05-11 17:55:07 +0200

    Tags next

--- client/tag/main.js
+++ client/tag/main.js
@@ -1,10 +1,10 @@
 // Pick out the unique tags from all todos in current list.
-Template.tag_filter.tags = function () {
-  var tag_infos = [];
-  var total_count = 0;
+Template.listTag.tags = function () {
+	var tag_infos = [];
+	var total_count = 0;
 
-  Todos.find({list_id: Session.get('list_id')}).forEach(function (todo) {
-    _.each(todo.tags, function (tag) {
+	Tasks.find().forEach(function (task) {
+	_.each(todo.tags, function (tag) {
       var tag_info = _.find(tag_infos, function (x) { return x.tag === tag; });
       if (! tag_info)
         tag_infos.push({tag: tag, count: 1});
@@ -18,21 +18,4 @@ Template.tag_filter.tags = function () {
   tag_infos.unshift({tag: null, count: total_count});
 
   return tag_infos;
-};
-
-Template.tag_filter.tag_text = function () {
-  return this.tag || "All items";
-};
-
-Template.tag_filter.selected = function () {
-  return Session.equals('tag_filter', this.tag) ? 'selected' : '';
-};
-
-Template.tag_filter.events({
-  'mousedown .tag': function () {
-    if (Session.equals('tag_filter', this.tag))
-      Session.set('tag_filter', null);
-    else
-      Session.set('tag_filter', this.tag);
-  }
-});
+}

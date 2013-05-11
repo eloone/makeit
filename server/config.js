6446ce8b9458a81db65184c084321b72d6f54031
Greg Bergé <neoziro@gmail.com>
2013-05-11 17:45:16 +0200

    add facebook login

---
+++ server/config.js
@@ -0,0 +1,9 @@
+Accounts.loginServiceConfiguration.remove({
+    service: 'facebook'
+});
+
+Accounts.loginServiceConfiguration.insert({
+    service: 'facebook',
+    appId  : '118830568318723',
+    secret : 'fa912cea15453e283b7944fc32a2e908'
+});

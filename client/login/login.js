834f645c464d3ef9a4142fe51ae1e69ba8c9b58f
Greg Bergé <neoziro@gmail.com>
2013-05-12 01:21:13 +0200

    css design

--- client/login/login.js
+++ client/login/login.js
@@ -1,11 +1,10 @@
-// Login to website
-login = function () {
-  Meteor.loginWithFacebook({}, function (err) {
-    if (err)
-      Session.set('errorMessage', err.reason || 'Unknown error');
-  });
-};
-
 Template.login.events({
-  'click button': login
+  'click a': function (event) {
+    event.preventDefault();
+
+    Meteor.loginWithFacebook({}, function (err) {
+      if (err)
+        Session.set('errorMessage', err.reason || 'Unknown error');
+    });
+  }
 });

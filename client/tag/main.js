c12c96d91e1079362739b7ad067bc03a4fa2e422
Adlen Afane <adlen.afane@gmail.com>
2013-05-11 20:00:04 +0200

    Tags count ok

--- client/tag/main.js
+++ client/tag/main.js
@@ -4,26 +4,25 @@ Template.listTag.tags = function () {
 	var total_count = 0;
 
 	Tasks.find().forEach(function (task) {
-	_.each(task.tags, function (tag) {
-    if (tag != '') {
-      var tag_info = _.find(tag_infos, function (x) { return x.tags === tag; });
+  	_.each(task.tags, function (tag) {
+      var tag_info = _.find(tag_infos, function (x) { return x.text === tag; });
         if (! tag_info)
           tag_infos.push({text: tag, count: 1});
         else
           tag_info.count++;
-    }});
+    });
     total_count++;
   });
 
   tag_infos = _.sortBy(tag_infos, function (x) { return x.tag; });
-  tag_infos.unshift({text: 'All items', count: total_count});
+  tag_infos.unshift({text: null, count: total_count});
 
   return tag_infos;
 }
 
-// Template.listTag.tag_text = function () {
-//   return this.text || "All items";
-// };
+Template.listTag.tag_text = function () {
+  return this.text || "All items";
+};
 
 Template.listTag.selected = function () {
   return Session.equals('listTag', this.text) ? 'selected' : '';

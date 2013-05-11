83cfc1b0d653533de11e88ddbe528223a57ba1b2
eloone <elodie.rafali@gmail.com>
2013-05-12 00:30:55 +0200

    rajout de tooltips sur les cursors

--- client/task/new/new.js
+++ client/task/new/new.js
@@ -17,6 +17,14 @@ getCursorScore = function (cursor) {
   return $('[data-type=' + cursor + '] .cursor.checked').length;
 };
 
+Template['new-task']['difficulty_tooltip'] = function(){
+    return Session.get('difficulty_tooltip');
+};
+
+Template['new-task']['likeness_tooltip'] = function(){
+    return Session.get('likeness_tooltip');
+};
+
 Template['new-task'].events({
   'keyup [name=task]': function (event) {
     if (event.keyCode !== 13) // 13 = enter
@@ -51,7 +59,9 @@ Template['new-task'].events({
   },
   'mouseleave .trigger': function(event){
     var $target = $(event.target);
-    $target.siblings().andSelf().removeClass('hover');   
+    $target.siblings().andSelf().removeClass('hover'); 
+    Session.set('difficulty_tooltip', null);
+    Session.set('likeness_tooltip', null);
   },
   'mouseenter .trigger' : function(event){
     var $target = $(event.target);
@@ -59,9 +69,28 @@ Template['new-task'].events({
         $target.prevAll().andSelf().addClass('hover');
         $target.nextAll().removeClass('hover');
         $target.css('cursor', 'pointer');
+        
+        if($target.hasClass('cursor')){
+            if($target.hasClass('icon-heart')){
+                Session.set('likeness_tooltip', $target.data('title'));
+            }
+            else{
+                Session.set('likeness_tooltip', null);
+            }
+             if($target.hasClass('icon-bolt')){
+                Session.set('difficulty_tooltip', $target.data('title'));
+            }
+            else{
+                Session.set('difficulty_tooltip', null);
+            }
+        }
+        
     }
     else{
         $target.css('cursor', 'default');
+        Session.set('difficulty_tooltip', null);
+        Session.set('likeness_tooltip', null);
     }
+
   }
 });

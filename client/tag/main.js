3753c12f9faa056b1298d4f18960022a37b6c5fb
Adlen Afane <adlen.afane@gmail.com>
2013-05-11 18:26:13 +0200

    Tags on click available

--- client/tag/main.js
+++ client/tag/main.js
@@ -16,7 +16,24 @@ Template.listTag.tags = function () {
   });
 
   tag_infos = _.sortBy(tag_infos, function (x) { return x.tag; });
-  // tag_infos.unshift({tag: null, count: total_count});
+  tag_infos.unshift({text: null, count: total_count});
 
   return tag_infos;
 }
+
+Template.listTag.tag_text = function () {
+  return this.text || "All items";
+};
+
+Template.listTag.selected = function () {
+  return Session.equals('listTag', this.text) ? 'selected' : '';
+};
+
+Template.listTag.events({
+  'mousedown .tag': function () {
+    if (Session.equals('listTag', this.text))
+      Session.set('listTag', null);
+    else
+      Session.set('listTag', this.text);
+  }
+});

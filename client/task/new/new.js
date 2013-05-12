31445b527336bd162af7141d9b82d62eb4c615bb
Greg Bergé <neoziro@gmail.com>
2013-05-12 02:23:18 +0200

    add suggestions

--- client/task/new/new.js
+++ client/task/new/new.js
@@ -17,12 +17,62 @@ getCursorScore = function (cursor) {
   return $('[data-type=' + cursor + '] .cursor.checked').length;
 };
 
-Template['new-task']['difficulty_tooltip'] = function(){
+// Get suggestions from a tag
+getSuggestions = function (tags) {
+  var suggestions = [];
+
+  _.each(tags, function (tag) {
+    var tasks = [],
+        text;
+
+    switch(tag.toLowerCase()) {
+      case 'roma':
+      text = 'Oh you are going to Roma ? Here are some things you might want to check:';
+      tasks = tasks.concat([
+        'Buy tickets',
+        'Booking Hotel',
+        'Prepare visits'
+        ]);
+    }
+
+    // Append tag
+    tasks = _.map(tasks, function (suggestion) {
+      return suggestion + ' #' + tag;
+    });
+
+    if (text)
+      suggestions.push({
+        text: text,
+        tasks: tasks
+      });
+  });
+
+  return suggestions;
+};
+
+addTask = function (options) {
+  Tasks.insert(_.extend({
+    user: Meteor.user()._id,
+    tags: extractHashTags(options.text),
+    satisfaction: 0,
+    difficulty: 0,
+    done: false
+  }, options, {
+    text: stripHashTags(options.text)
+  }));
+};
+
+
+Template['new-task'].difficulty_tooltip = function(){
     return Session.get('difficulty_tooltip');
 };
 
-Template['new-task']['likeness_tooltip'] = function(){
+Template['new-task'].likeness_tooltip = function(){
     return Session.get('likeness_tooltip');
+};
+
+Template['new-task'].suggestions = function () {
+  return Session.get('suggestions');
 };
 
 Template['new-task'].events({
@@ -31,35 +81,44 @@ Template['new-task'].events({
       return true;
 
     var $target = $(event.target),
-        text = $target.val();
+        text = $target.val(),
+        tags = extractHashTags(text);
 
     // Insert task
-    Tasks.insert({
-      user: Meteor.user()._id,
-      text: stripHashTags(text),
-      tags: extractHashTags(text),
+    addTask({
+      text: text,
       satisfaction: getCursorScore('satisfaction'),
-      difficulty: getCursorScore('difficulty'),
-      done: false
+      difficulty: getCursorScore('difficulty')
     });
 
     // Clear input
     $target.val('');
+
     //Clear cursors
     $('.task-creator .cursor').removeClass('checked');
-    
+
+    //Set suggestions
+    Session.set('suggestions', getSuggestions(tags));
   },
 
   'click .cursor': function (event) {
-    var $target = $(event.target);
-    if($('input[name="task"]').val() != ''){
-        $target.prevAll().andSelf().addClass('checked');
-        $target.nextAll().removeClass('checked');
-    }
+    if(! $('input[name="task"]').val())
+      return ;
+
+    $(event.target)
+    .prevAll().andSelf().addClass('checked')
+    .end().nextAll().removeClass('checked');
   },
+
+  'click .task-list li': function (event) {
+    var $target = $(event.currentTarget);
+    addTask({text: $target.data('text')});
+    $target.fadeOut();
+  },
+
   'mouseleave .trigger': function(event){
     var $target = $(event.target);
-    $target.siblings().andSelf().removeClass('hover'); 
+    $target.siblings().andSelf().removeClass('hover');
     Session.set('difficulty_tooltip', null);
     Session.set('likeness_tooltip', null);
   },
@@ -69,7 +128,7 @@ Template['new-task'].events({
         $target.prevAll().andSelf().addClass('hover');
         $target.nextAll().removeClass('hover');
         $target.css('cursor', 'pointer');
-        
+
         if($target.hasClass('cursor')){
             if($target.hasClass('icon-heart')){
                 Session.set('likeness_tooltip', $target.data('title'));
@@ -84,7 +143,7 @@ Template['new-task'].events({
                 Session.set('difficulty_tooltip', null);
             }
         }
-        
+
     }
     else{
         $target.css('cursor', 'default');
